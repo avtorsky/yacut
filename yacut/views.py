d2ec7efa54122b4807daaf403bb5b926752eb8d6
n.rurenko <eavtorsky@gmail.com>
2023-02-19 06:25:56 +0300

    fix(./yacut/views.py): PEP8 linting patch

--- yacut/views.py
+++ yacut/views.py
@@ -8,9 +8,9 @@ from .forms import YacutForm
 from .models import URLMap
 
 READY_SHORT_URL = (
-            'Ваша новая ссылка готова: '
-            '<a href="{short_url}">{short_url}</a>'
-        )
+  'Ваша новая ссылка готова: '
+  '<a href="{short_url}">{short_url}</a>'
+)
 
 
 def get_unique_short_id():
